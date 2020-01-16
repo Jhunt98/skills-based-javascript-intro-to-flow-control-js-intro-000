404eceddd11ed7ea982d3b4a2f499d29a763316f
Joshua Hunt <jelijahhunt@gmail.com>
2020-01-16 22:40:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case age>12 && age<20:
+  return 'You are a teenager'
+  default: 
+  return 'You have an age'
+}
 }
